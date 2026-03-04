bbd014dae717fa14521c93639523b2acc79ee04f
yasminulfah <ulfahmi1719@gmail.com>
2026-03-05 06:57:12 +0700

    Feat: welcome to my backend server

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'project' => 'Yasmin Mini Wallet',
+        'developer' => 'Syarifatul Yasmin Ulfah',
+        'status' => 'Live',
+        'version' => '1.0.0',
+        'message' => 'Backend is running smoothly! ✨'
+    ]);
 });
